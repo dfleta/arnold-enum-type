724dfff263297a25b05ac64a6fe55064dedb9f9b
David Gelpi <gelpiorama@gmail.com>
2024-04-08 20:37:34 +0200

    build(pom): Actualizar versiones dependencias

--- src/main/java/org/foobarspam/arnoldEnumType/main/ArnoldMain.java
+++ src/main/java/org/foobarspam/arnoldEnumType/main/ArnoldMain.java
@@ -4,23 +4,23 @@ import org.foobarspam.arnoldEnumType.logica.Planeta;
 
 public class ArnoldMain {
 	
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 		 
-		 double peso = 1.0; // kg
+		double peso = 1.0; // kg
 		 
-         for(Planeta planeta : Planeta.values()){
-        	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-         }
+        for(Planeta planeta : Planeta.values()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
          
-         System.out.println("\nYour weight only on the terrestrial planets: ");
-         for(Planeta planeta: Planeta.getPlanetasTerrestres()){
-        	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-         }
+        System.out.println("\nYour weight only on the terrestrial planets: ");
+        for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
          
-         System.out.println("\nYour weight only on the gas giant planets: ");
-         for(Planeta planeta: Planeta.getGigantesGaseosos()){
-        	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-         }
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for(Planeta planeta: Planeta.getGigantesGaseosos()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
 	 }
 
 }
